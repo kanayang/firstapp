6d9c9a8a12f9063d6035080a692ef5f4ca378681
kanayang <shu.k47@gmail.com>
2022-10-15 14:30:17 +0900

    first

---
+++ pages/firstpage.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+export default function HelloWorld() {
+    return (
+        <div>
+            <Head>
+                <title>はじめてのNextjs</title>
+                <meta name="description" content="はじめてのNextjs" />
+            </Head>
+            <h1>はじめてのNextjs</h1>
+        </div>
+    )
+}
